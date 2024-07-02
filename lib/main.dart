e2d9cd4f62cf74443f0b1c71f9c178b553ad50c5
Over_Brave <47111280+OverBrave@users.noreply.github.com>
2024-07-02 14:07:38 +0300

    Auth pages added.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travelguide/pages/auth/login_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +12,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Firebase Auth Test")),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              try {
-                UserCredential userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: "testuser@example.com",
-                        password: "testpassword");
-                print('User created: ${userCredential.user?.email}');
-              } catch (e) {
-                print('Error creating user: $e');
-              }
-            },
-            child: Text('Create Test User'),
+      title: 'Flutter Auth',
+      theme: ThemeData(
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: const Color(0xFF2fa4cf), // Gök mavisi
+            elevation: 10, // Gölge yüksekliği
+            textStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 21, color: Colors.white), // Beyaz yazı (font
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(30),
+            ),
+            padding: const EdgeInsets.symmetric(horizontal: 96, vertical: 12),
+          ),
+        ),
+        textButtonTheme: TextButtonThemeData(
+          style: TextButton.styleFrom(
+            foregroundColor: const Color(0xFFd38c40),
+            textStyle: const TextStyle(fontWeight: FontWeight.bold, fontSize: 21), // Gök mavisi
+          ),
+        ),
+        inputDecorationTheme: InputDecorationTheme(
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(30),
           ),
         ),
       ),
+      home: LoginScreen(),
     );
   }
 }
