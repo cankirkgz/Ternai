f098a026c4006f6cff596d8fc2b145677ff8ae2c
“Mucahit <mgokceteoman@gmail.com>
2024-07-02 16:52:26 +0300

    SignUp page-beta v0.01

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,11 @@
 // import 'package:flutter/material.dart';
 // import 'package:flutter_test/flutter_test.dart';
 
+<<<<<<< HEAD
 // import 'package:travelguide/main.dart';
+=======
+import 'package:travelguide/src/app.dart';
+>>>>>>> 4fdcc4e (SignUp page-beta v0.01)
 
 // void main() {
 //   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
