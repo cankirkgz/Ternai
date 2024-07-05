f1433d7ebf3d885df93133483a8c9fc080df5b3c
İrem <86803958+iremnuromgen@users.noreply.github.com>
2024-07-05 16:07:28 +0300

    login page UI completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:travelguide/views/login_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -12,25 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(title: Text("Firebase Auth Test")),
-        body: Center(
-          child: ElevatedButton(
-            onPressed: () async {
-              try {
-                UserCredential userCredential = await FirebaseAuth.instance
-                    .createUserWithEmailAndPassword(
-                        email: "testuser@example.com",
-                        password: "testpassword");
-                print('User created: ${userCredential.user?.email}');
-              } catch (e) {
-                print('Error creating user: $e');
-              }
-            },
-            child: Text('Create Test User'),
-          ),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
     );
   }
 }
