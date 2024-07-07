05849774f6d11c2cf27e5ee54315df1dcef7706d
Can Kırkgöz <mcankirkgoz@gmail.com>
2024-07-07 15:07:16 +0300

    import silindi

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:travelguide/views/onboarding_screens/onboarding_view.dart';
 
 void main() async {
