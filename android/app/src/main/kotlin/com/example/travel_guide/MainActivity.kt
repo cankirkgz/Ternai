9f6a0a16fd5f4d579902cd196149cd274d6b2a95
“Mucahit <mgokceteoman@gmail.com>
2024-07-07 19:28:21 +0300

    reset

--- android/app/src/main/kotlin/com/example/travel_guide/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travel_guide
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- android/app/src/main/kotlin/com/example/travelguide/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.travelguide
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
