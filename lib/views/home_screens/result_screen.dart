4a2594217fcc9a4c545497952be6a48d5a8c25e9
Can Kırkgöz <mcankirkgoz@gmail.com>
2024-07-21 14:22:51 +0300

    fiyat arama sayfası güncellendi

--- lib/views/home_screens/result_screen.dart
+++ lib/views/home_screens/result_screen.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/material.dart';
 import 'package:currency_converter/currency.dart';
 import 'package:currency_converter/currency_converter.dart';
-import 'package:flutter/material.dart';
 import 'package:travelguide/models/price_model.dart';
 import 'package:travelguide/models/country_model.dart';
 import 'package:travelguide/models/category_model.dart';
@@ -11,12 +11,14 @@ class ResultScreen extends StatefulWidget {
   final Country country;
   final Category category;
   final ProductService? subCategory;
+  final Country? userCountry; // Kullanıcının ülkesi
 
   const ResultScreen({
     Key? key,
     required this.country,
     required this.category,
     this.subCategory,
+    required this.userCountry, // Kullanıcının ülkesi
   }) : super(key: key);
 
   @override
@@ -35,18 +37,38 @@ class _ResultScreenState extends State<ResultScreen> {
         widget.country.id, widget.category.id, widget.subCategory?.id);
   }
 
-  Future<double?> convertCurrency(
-      String fromCurrency, String toCurrency, double amount) async {
+  Future<Map<String, double>> convertCurrencies(
+      String fromCurrency, double amount) async {
     try {
-      var rate = await CurrencyConverter.convert(
-        from: Currency.values.firstWhere((e) => e.name == fromCurrency),
-        to: Currency.values.firstWhere((e) => e.name == toCurrency),
+      print(
+          "Converting from $fromCurrency to USD and ${widget.userCountry!.currency}");
+
+      var usdRate = await CurrencyConverter.convert(
+        from: Currency.values.firstWhere((e) => e.name == fromCurrency,
+            orElse: () => Currency.usd),
+        to: Currency.usd,
         amount: amount,
       );
-      return rate;
+
+      var userCurrencyRate = await CurrencyConverter.convert(
+        from: Currency.values.firstWhere((e) => e.name == fromCurrency,
+            orElse: () => Currency.usd),
+        to: Currency.values.firstWhere(
+            (e) => e.name == widget.userCountry!.currency,
+            orElse: () => Currency.usd),
+        amount: amount,
+      );
+
+      print(
+          "Converted rates: USD = $usdRate, User Currency = $userCurrencyRate");
+
+      return {
+        'usd': usdRate ?? 0.0,
+        'userCurrency': userCurrencyRate ?? 0.0,
+      };
     } catch (e) {
       print("Currency conversion error: $e");
-      return null;
+      return {};
     }
   }
 
@@ -74,20 +96,14 @@ class _ResultScreenState extends State<ResultScreen> {
               itemCount: prices.length,
               itemBuilder: (context, index) {
                 Price price = prices[index];
-                return FutureBuilder<double?>(
-                  future: convertCurrency(price.currency, 'TRY', price.price),
+                return FutureBuilder<Map<String, double>>(
+                  future: convertCurrencies(price.currency, price.price),
                   builder: (context, conversionSnapshot) {
                     if (conversionSnapshot.connectionState ==
                         ConnectionState.waiting) {
-                      return ListTile(
-                        title: Text(
-                          '${price.productServiceName} - ${price.price} ${price.currency}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        subtitle: Text('Çeviriliyor...'),
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Center(child: CircularProgressIndicator()),
                       );
                     } else if (conversionSnapshot.hasError) {
                       return ListTile(
@@ -101,45 +117,77 @@ class _ResultScreenState extends State<ResultScreen> {
                         subtitle: Text('Döviz çevirme hatası'),
                       );
                     } else {
-                      double? convertedPrice = conversionSnapshot.data;
-                      double? usdPrice = convertedPrice != null
-                          ? (convertedPrice / 26.88)
-                          : null; // 1 USD = 26.88 TRY (örnek oran)
-                      return ListTile(
-                        title: Text(
-                          '${price.productServiceName}',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
+                      var conversionRates = conversionSnapshot.data!;
+                      double? usdPrice = conversionRates['usd'];
+                      double? userCurrencyPrice =
+                          conversionRates['userCurrency'];
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 16.0),
+                        child: Card(
+                          elevation: 4.0,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(12.0),
                           ),
-                        ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            SizedBox(height: 8),
-                            Text(
-                              'Ülke Fiyatı: ${price.price.toStringAsFixed(2)} ${price.currency}',
-                              style: TextStyle(
-                                fontSize: 16,
-                                color: Colors.blue,
-                              ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                Text(
+                                  '${widget.country.name} - ${price.productServiceName}',
+                                  style: TextStyle(
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Ülke Fiyatı: ${price.price.toStringAsFixed(2)} ${price.currency}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.blue,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  'Dolar Fiyatı: ${usdPrice?.toStringAsFixed(2)} USD',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.green,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                Text(
+                                  '${widget.userCountry!.name} Fiyatı: ${userCurrencyPrice?.toStringAsFixed(2)} ${widget.userCountry!.currency}',
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                    color: Colors.orange,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Son güncelleme: ${price.lastUpdated.day}/${price.lastUpdated.month}/${price.lastUpdated.year}',
+                                  style: TextStyle(
+                                    fontSize: 14,
+                                    fontStyle: FontStyle.italic,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                                SizedBox(height: 8),
+                                Text(
+                                  'Not: Bu fiyatlar ortalama olup, yukarı veya aşağı yönlü değişiklik gösterebilir.',
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.redAccent,
+                                  ),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ],
                             ),
-                            Text(
-                              'Son güncelleme: ${price.lastUpdated.day}/${price.lastUpdated.month}/${price.lastUpdated.year}',
-                              style: TextStyle(
-                                fontSize: 14,
-                                fontStyle: FontStyle.italic,
-                              ),
-                            ),
-                            SizedBox(height: 8),
-                            Text(
-                              'Not: Bu fiyatlar ortalama olup, yukarı veya aşağı yönlü değişiklik gösterebilir.',
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.redAccent,
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
                       );
                     }
